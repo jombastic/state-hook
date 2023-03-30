30bebc7bf0c8d66b2706d00a801cad00cc16e43b
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-03-30 16:44:13 +0200

    change the post in DataFetching when user inputs another value and only clicks a button to fetch post

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -4,6 +4,11 @@ import React, { useEffect, useState } from 'react'
 const DataFetching = () => {
     const [post, setPost] = useState({})
     const [id, setId] = useState(1)
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1)
+
+    const handleClick = () => {
+        setIdFromButtonClick(id)
+    }
 
     useEffect(() => {
         axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -14,11 +19,12 @@ const DataFetching = () => {
             .catch(err => {
                 console.log(err)
             })
-    }, [id])
+    }, [idFromButtonClick])
 
     return (
         <div>
             <input type="text" value={id} onChange={e => setId(e.target.value)} />
+            <button type='button' onClick={handleClick}>Fetch Post</button>
             <div>{post.title}</div>
             {/* <ul>
                 {
