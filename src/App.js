1f732b0630228ad0bbc86e9da852ee9a88a363ff
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-03-30 15:18:54 +0200

    call useEffect only once in HookMouse component to add event listener for mouse move

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
+import ClassMouse from './components/ClassMouse';
 import HookCounter from './components/HookCounter';
 import HookCounterFour from './components/HookCounterFour';
 import HookCounterOne from './components/HookCounterOne';
 import HookCounterThree from './components/HookCounterThree';
 import HookCounterTwo from './components/HookCounterTwo';
+import HookMouse from './components/HookMouse';
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter /> */}
+      {/* <ClassMouse /> */}
+      <HookMouse />
       {/* <HookCounter /> */}
-      <HookCounterOne />
+      {/* <HookCounterOne /> */}
       {/* <HookCounterTwo /> */}
       {/* <HookCounterThree /> */}
       {/* <HookCounterFour /> */}
---
+++ src/components/ClassMouse.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class ClassMouse extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            x: 0,
+            y: 0
+        }
+    }
+
+    logMouseposition = e => {
+        this.setState({ x: e.clientX, y: e.clientY })
+    }
+
+    componentDidMount() {
+        window.addEventListener('mousemove', this.logMouseposition)
+    }
+
+    render() {
+        return (
+            <div>
+                X - {this.state.x} Y - {this.state.y}
+            </div>
+        )
+    }
+}
