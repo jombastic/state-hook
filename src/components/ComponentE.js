6006a9059acc15164c52d4bb02483dfb4716a7cc
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-03-31 10:38:15 +0200

    use the useContext function in ComponentE

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ChanelContext, UserContext } from '../App'
 import ComponentF from './ComponentF'
 
 const ComponentE = () => {
+  const user = useContext(UserContext)
+  const chanel = useContext(ChanelContext)
+
   return (
     <div>
-        <ComponentF />
+      {user} - {chanel}
     </div>
   )
 }
